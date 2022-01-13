1692bcae0e6b6ab8865f14a4afe53f15d3a41aed
york111q <york111q@gmail.com>
2022-01-13 14:50:43 +0100

    28| Filters cleanup

--- siatka/zapisy/templatetags/filters.py
+++ siatka/zapisy/templatetags/filters.py
@@ -1,19 +1,11 @@
 from django import template
 from django.utils.safestring import mark_safe
-from django.utils.text import slugify
 register = template.Library()
 
-@register.filter
-def img_url(text):
-    return "zapisy/images/"+slugify(text)+".jpg"
 
 @register.filter
 def show_date(date):
     return date.strftime('%Y-%m-%d %H:%M')
-
-@register.filter
-def multiply(value, multiplier):
-    return value*multiplier
 
 @register.filter
 def show_price(value):
@@ -28,14 +20,6 @@ def show_colored_price(value):
         return mark_safe(f'<span class="text-danger">{value_str}</span>')
     else:
         return mark_safe(f'<span class="text-warning">{value_str}</span>')
-
-@register.filter
-def entry_count_fee(object):
-    return str("{:.2f}".format(object.count_total_fee())).replace(".", ",") + 'zł'
-
-@register.filter
-def check_balance(object):
-    return object.count_balance()
 
 @register.filter(is_safe=True)
 def bool_symbol(bool):
